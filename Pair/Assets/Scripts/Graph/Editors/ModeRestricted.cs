c6c1609745f5b69905c2346dfde47d2d64acee30
craus <craus902@gmail.com>
2018-09-07 10:12:38 +0400

    zoom+pan for maze view; mode restricted controls

---
+++ Pair/Assets/Scripts/Graph/Editors/ModeRestricted.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using System;
+using UnityEngine.PostProcessing;
+
+public class ModeRestricted : MonoBehaviour {
+	public GameObject target;
+	public UI.Mode mode;
+
+	public void Update() {
+		target.SetActive(mode == UI.instance.mode);
+	}
+}
---
+++ Pair/Assets/Scripts/Graph/Editors/Viewer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using System;
+
+public class Viewer : MonoBehaviour {
+	
+	public Vector2 cameraDragRelativePosition;
+
+	public bool panned = false;
+
+	public new Camera camera;
+
+	public Vector2 Mouse {
+		get {
+			return camera.ScreenToWorldPoint(Input.mousePosition);
+		}
+	}
+
+	protected void Zoom(float times) {
+		var oldMouse = Mouse;
+		camera.orthographicSize /= times;
+		camera.transform.localScale /= times;
+		camera.transform.position += (oldMouse - Mouse).WithZ(0);
+	}
+
+	protected void Zoom() {
+		Zoom(Mathf.Pow(2f, Input.GetAxis("Mouse ScrollWheel")));
+	}
+
+	protected void Pan() {
+		if (Input.GetButtonDown("Pan")) {
+			cameraDragRelativePosition = Mouse;
+			panned = false;
+		}
+		if (Input.GetButton("Pan")) {
+			var delta = (cameraDragRelativePosition - Mouse).WithZ(0);
+			camera.transform.position += delta;
+			if (delta != Vector3.zero) {
+				panned = true;
+			}
+		}
+		if (Input.GetButtonUp("Pan")) {
+			panned = false;
+		}
+	}
+
+	public void Update() {
+		Pan();
+		Zoom();
+	}
+}
